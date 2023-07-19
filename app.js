4dbb3b87d1be8a61d3e05f0a832af05f0771e526
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-07-19 21:34:50 +0800

    Completyed 16x16 grid, added prompt button and created hover event.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
 
-
-function creategrid(width) {
- for (let i = 0; i <= width; i++) {
-    const createBox = document.createElement("div");
-    createBox.textContent = `${i}`;
-    createBox.classList.add("gridCube");
-    container.appendChild(createBox);
- }
+function creategrid(width, height) {
+ for (let i = 1; i <= (width*height); i++) {
+   const createBox = document.createElement("div");
+   createBox.textContent = `${i}`;
+   createBox.classList.add("gridCube");
+   container.appendChild(createBox);
+   createBox.addEventListener("mouseover", () => {
+      createBox.classList.add ("gridHover");
+   });
+ };
 };
 
-creategrid (16)
+creategrid (16, 16)
+
+button.addEventListener ("click", () => {
+   let change = prompt ("Change grid size to?", "16");
+   creategrid (change, change)
+   })
+
