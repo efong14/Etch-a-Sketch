0f66d7d97287c2464e3357e2d1d68d37befcdde5
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-07-20 22:18:07 +0800

    Remove certain unnecessary items in css file, allowed re-size button to clear previously made grid and set box sizes in grid to change based on number of boxes to fit into the container.

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
 
-
 function creategrid(width, height) {
+let boxWidth = 100/width;
+let boxHeight = 100/height;
  for (let i = 1; i <= (width*height); i++) {
    const createBox = document.createElement("div");
-   createBox.textContent = `${i}`;
    createBox.classList.add("gridCube");
    container.appendChild(createBox);
+   createBox.style.width = `${boxWidth}%`;
+   createBox.style.height = `${boxHeight}%`;
    createBox.addEventListener("mouseover", () => {
       createBox.classList.add ("gridHover");
    });
  };
 };
 
-creategrid (16, 16)
-
 button.addEventListener ("click", () => {
    let change = prompt ("Change grid size to?", "16");
-   creategrid (change, change)
-   })
+   if (change <= 100) {
+   container.textContent = "";
+   creategrid (change, change);
+   } else {
+      alert("Please enter a number below 100!");
+   }
+});
 
+creategrid (16, 16);
